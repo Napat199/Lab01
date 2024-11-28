6cb0659ab0d439a6560710edbffdeae197ed038a
Napat199 <s6704062611328@email.kmutnb.ac.th>
2024-11-28 14:54:39 +0700

    Update minx.c

--- minx.c
+++ minx.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
 #include <stdio.h>
 int main() {
-	int a = 0;
-	int b = 1;
+	int a;
+	int b;
+	printf("Input number : ");
+	scanf("%d", &a);
+	printf("Input number : ");
+	scanf("%d", &b);
 	if (a>b){
-		printf("Number 1 greater than Number 2");
+	    int g = a - b;
+		printf("%d %s %d %s %d", a," more than ",b," = ",g);
 	}
 	else {
-		printf("Number 2 greater than Number 1");
+	    int g = b - a;
+		printf("%d %s %d %s %d", b," more than ",a," = ",g);
 	}	
 	return 0;
 }
